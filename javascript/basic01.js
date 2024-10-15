0f644b840ad90dd80d13e9dbfc0e44fc54e69bf7
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-10-15 14:59:33 +0900

    javascript 기초 맛보기

---
+++ javascript/basic01.js
@@ -0,0 +1,24 @@
+// 변수 (let, const) const:중간에 뭘 바꾸는것을 방지하기 위해 사용
+// 변하는 수. 값이 변하는 데이터를 저장하고 관리하기 위한 공간
+// 예약어
+
+let a = 10; // a라는 변수를 선언하고 그 안에 값 10을 저장한다.
+let b; // b변수 선언
+
+b = 15; //b에 15라는 값을 저장(할당)한다.
+
+// let let; 
+// let my school;
+let my_school;
+let mySchool;
+let todayLunchTime;
+
+let memu = "햄버거"; //문자열은 "" or ''로 감싸준다
+
+let number = 150.123;
+
+const cup = '콜라';
+
+number = 221.1; // 새로 담기
+
+// cup = '사이다'; 안됨
